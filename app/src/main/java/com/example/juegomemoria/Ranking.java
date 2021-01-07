6b3f305495e1d137d59cde65b29d8de9e9c491c6
Federico Aguilera <federico.aguilera@est.fi.uncoma.edu.ar>
2021-01-07 12:31:11 -0300

    Initial commit

---
+++ app/src/main/java/com/example/juegomemoria/Ranking.java
@@ -0,0 +1,14 @@
+package com.example.juegomemoria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ranking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ranking);
+    }
+}
