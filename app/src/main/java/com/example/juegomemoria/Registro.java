d0aa3c91b1374da300456409a6740ef1a1d42790
Felipe Vidal Soto <felipe.vidal@est.fi.uncoma.edu.ar>
2021-01-09 12:19:50 -0300

    Validaciones del registro

--- app/src/main/java/com/example/juegomemoria/Registro.java
+++ app/src/main/java/com/example/juegomemoria/Registro.java
@@ -3,12 +3,38 @@ package com.example.juegomemoria;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Registro extends AppCompatActivity {
+
+    private EditText txt_nombre, txt_apellido, txt_username, txt_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        txt_nombre = (EditText) findViewById(R.id.nombre);
+        txt_apellido = (EditText) findViewById(R.id.apellido);
+        txt_username = (EditText) findViewById(R.id.username);
+        txt_password = (EditText) findViewById(R.id.password);
+
     }
+
+    public void clickRegistro(View view) {
+
+        String nombre = txt_nombre.getText().toString();
+        String apellido = txt_apellido.getText().toString();
+        String username = txt_username.getText().toString();
+        String password = txt_password.getText().toString();
+
+        if (nombre.length() == 0 || apellido.length() == 0 || username.length() == 0 || password.length() == 0) {
+            Toast.makeText(this, "Faltan completar campos", Toast.LENGTH_LONG).show();
+        } else Toast.makeText(this, "Registro en proceso..", Toast.LENGTH_LONG).show();
+
+    //Validar con BD usuario
+    }
+
 }
