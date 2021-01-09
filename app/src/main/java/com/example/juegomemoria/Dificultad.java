ba95ead06ddce2789a926a4afdf2ece2ab635ad4
Federico Aguilera <federico.aguilera@est.fi.uncoma.edu.ar>
2021-01-09 14:14:09 -0300

    Comportamiento de botones y musica agregado

--- app/src/main/java/com/example/juegomemoria/Dificultad.java
+++ app/src/main/java/com/example/juegomemoria/Dificultad.java
@@ -1,20 +1,29 @@
 package com.example.juegomemoria;
 
+import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
-public class Dificultad extends AppCompatActivity {
+public class Dificultad extends AppCompatActivity implements View.OnClickListener {
 
     Button btnFacil;
     Button btnNormal;
     Button btnDificil;
     Button btnJugar;
-    Button btnSonido;
+    ToggleButton btnSonido;
     Button btnRanking;
     ImageView btnAtras;
+    int eleccion = 0;
+    AudioManager amanager;
 
 
     @Override
@@ -22,12 +31,71 @@ public class Dificultad extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dificultad);
 
+        amanager = (AudioManager)getSystemService(AUDIO_SERVICE);
+
         btnFacil = (Button) findViewById(R.id.btnFacil);
         btnNormal = (Button) findViewById(R.id.btnNormal);
         btnDificil = (Button) findViewById(R.id.btnDificil);
         btnJugar = (Button) findViewById(R.id.btnJugar);
-        btnSonido = (Button) findViewById(R.id.btnSonido);
+        btnSonido = (ToggleButton) findViewById(R.id.btnSonido);
         btnRanking = (Button) findViewById(R.id.btnRanking);
         btnAtras = (ImageView) findViewById(R.id.btnAtras);
+        btnSonido.setChecked(true);
+
+        btnFacil.setOnClickListener(this);
+        btnNormal.setOnClickListener(this);
+        btnDificil.setOnClickListener(this);
+        btnJugar.setOnClickListener(this);
+        btnSonido.setOnClickListener(this);
+        btnRanking.setOnClickListener(this);
+        btnAtras.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnFacil:
+                eleccion = 1;
+                btnFacil.setBackgroundColor(ContextCompat.getColor(getBaseContext(), R.color.btnDifActivado));
+                btnNormal.setBackgroundColor(ContextCompat.getColor(getBaseContext(), R.color.btnDifDesactivado));
+                btnDificil.setBackgroundColor(ContextCompat.getColor(getBaseContext(), R.color.btnDifDesactivado));
+                break;
+            case R.id.btnNormal:
+                eleccion = 2;
+                btnFacil.setBackgroundColor(ContextCompat.getColor(getBaseContext(), R.color.btnDifDesactivado));
+                btnNormal.setBackgroundColor(ContextCompat.getColor(getBaseContext(), R.color.btnDifActivado));
+                btnDificil.setBackgroundColor(ContextCompat.getColor(getBaseContext(), R.color.btnDifDesactivado));
+                break;
+            case R.id.btnDificil:
+                eleccion = 3;
+                btnFacil.setBackgroundColor(ContextCompat.getColor(getBaseContext(), R.color.btnDifDesactivado));
+                btnNormal.setBackgroundColor(ContextCompat.getColor(getBaseContext(), R.color.btnDifDesactivado));
+                btnDificil.setBackgroundColor(ContextCompat.getColor(getBaseContext(), R.color.btnDifActivado));
+                break;
+            case R.id.btnJugar:
+                if (eleccion != 0) {
+                    Intent i = new Intent(Dificultad.this, Juego.class);
+                    startActivity(i);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Debes elegir una dificultad", Toast.LENGTH_LONG).show();
+                }
+
+                break;
+            case R.id.btnSonido:
+                if(btnSonido.isChecked())
+                    amanager.setStreamMute(AudioManager.STREAM_MUSIC, false);
+                else
+                    amanager.setStreamMute(AudioManager.STREAM_MUSIC, true);
+                break;
+            case R.id.btnRanking:
+                Intent i = new Intent(Dificultad.this, Ranking.class);
+                startActivity(i);
+                break;
+            case R.id.btnAtras:
+                onBackPressed();
+                break;
+        }
+        ;
+    }
+
 }
--- app/src/main/java/com/example/juegomemoria/MainActivity.java
+++ app/src/main/java/com/example/juegomemoria/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.juegomemoria;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,11 +16,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     EditText user, pass;
     Button btnLogin, btnRegister;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        mediaPlayer = new MediaPlayer();
+        playMP();
 
         user = (EditText) findViewById(R.id.loginUserName);
         pass = (EditText) findViewById(R.id.loginPassword);
@@ -26,6 +34,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnLogin.setOnClickListener(this);
         btnRegister.setOnClickListener(this);
+
+
     }
 
     @Override
@@ -45,5 +55,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(i);
                 break;
         }
+    }
+
+    public void playMP(){
+        Thread playThread = new Thread(){
+            public void run(){
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.music);
+                mediaPlayer.start();
+            }
+        };
+        playThread.start();
+    }
+
+    public void stopMP(){
+        mediaPlayer.stop();
+    }
+
+    public MediaPlayer getMediaPlayer() {
+        return mediaPlayer;
     }
 }
