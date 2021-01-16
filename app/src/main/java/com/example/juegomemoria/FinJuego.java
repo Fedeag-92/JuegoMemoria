23a0110bc982e456c5197f323e8bf941fabe8aa5
Mariano Palavecino <marianopalavecino7@gmail.com>
2021-01-16 16:23:57 -0300

    se mandan datos del juego por parametro

--- app/src/main/java/com/example/juegomemoria/FinJuego.java
+++ app/src/main/java/com/example/juegomemoria/FinJuego.java
@@ -3,12 +3,24 @@ package com.example.juegomemoria;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class FinJuego extends AppCompatActivity {
-
+    private int points, record, difficulty,errors;
+    private static final String TAG = "MyActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fin_juego);
+
+        points = getIntent().getIntExtra("points", 0);
+        record = getIntent().getIntExtra("record", 0);
+        difficulty = getIntent().getIntExtra("difficulty", 0);
+        errors = getIntent().getIntExtra("errors", 0);
+
+        Log.i(TAG,"PUNTOS"+points);
+        Log.i(TAG,"RECORD"+record);
+        Log.i(TAG,"DIFICULTAD"+difficulty);
+        Log.i(TAG,"ERRORES"+errors);
     }
 }
